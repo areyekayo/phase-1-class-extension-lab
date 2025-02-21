3ba8e6056d904aa7b72124cf5c0a5495b70643e0
Riko <riko.fluchel@gmail.com>
2025-02-21 08:26:22 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Your code here
+class Polygon {
+    constructor(sidesArray){
+        this.sides = sidesArray
+    }
+
+    get countSides(){
+        return this.sides.length
+    }
+
+    get perimeter(){
+        return this.sides.reduce((accum, value) => accum + value, 0);
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        /* 
+        Take three sides of a triangle and add them in pairs.
+        The sum of any two sides is greater than (not equal to) the third side.
+        If that is not true, then it is not possible to construct a triangle
+        with the given side lengths
+        */
+        const [a, b, c] = this.sides;
+
+        return (a + b > c) && (b + c > a) && (c + a > b)
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        /* 
+        Squares are valid when the lengths of all sides are equal
+        */
+        const [a,b,c,d] = this.sides
+
+        return (a === b && a === c && a === d)
+    }
+
+    get area() {
+        const [a,b,c,d] = this.sides
+        return a * b
+    }
+}
